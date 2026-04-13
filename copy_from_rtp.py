193f840725cc3573c7b74aa5245bd65f6183bf13
Kester Allen <kester@gmail.com>
2026-04-13 11:58:33 -0700

    updates to search UI

--- copy_from_rtp.py
+++ copy_from_rtp.py
@@ -1,16 +1,22 @@
 
 import datetime
-import requests
-import random
+import glob
 import json
+import random
+import requests
+import time
+
+my_path = "/home/kester/Pictures"
+pictures = glob.glob(f"{my_path}/**/*.jpg", recursive=True)
 
 rtp_geojson = "./plaques.geojson"
 with open(rtp_geojson) as file:
     rtp_data = json.load(file)
 
 now = datetime.datetime.now()
-url = "http://127.0.0.1:5000/submit"
-approve_url = "http://127.0.0.1:5000/admin/approve/all"
+base_url = "https://readtheplaque-standalone.fly.dev"
+url = f"{base_url}/submit"
+approve_url = f"{base_url}/admin/approve/all"
 fields = ("slug", "title", "description", "latitude", "longitude")
 
 for rtp_plaque in random.choices(rtp_data["features"], k=50):
@@ -19,14 +25,16 @@ for rtp_plaque in random.choices(rtp_data["features"], k=50):
     slug = props["title_page_url"].split("/")[2]
     title = props["title"]
     description = props.get("description", "")
-    print(slug, description)
     latitude = rtp_plaque["geometry"]["coordinates"][1]
     longitude = rtp_plaque["geometry"]["coordinates"][0]
 
-    image_filename = "/mnt/c/Users/CIAE/OneDrive - Novonesis/Pictures/Screenshots/Screenshot 2025-05-08 142354.png"
+    image_filename = random.choice(pictures)
+    print(slug, image_filename)
     with open(image_filename, "rb") as file:
         seed = (slug, title, description, latitude, longitude)
         files = {"images": (image_filename, file)}
         response = requests.post(url, data=dict(zip(fields, seed)), files=files)
+        print(response)
+        time.sleep(30)
 
 response = requests.get(approve_url)
